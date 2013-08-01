e5840f4799ed0ea63663f2570ed71656d73dc679
Phil Lavin <phil@propcom.co.uk>
2013-08-01 22:43:27 +0000

    First release

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-raspberry-pi-gpio-interrupt
-===========================
+Intro
+=====
 
-Example written in C demonstrating the use of kernel GPIO interrupts with jitter filtering
+Small C programme to demonstrate how to use kernel level GPIO interrupts on a Raspberry Pi. Also includes time based jitter filtering.
+
+Compiling
+---------
+
+* Clone the repository
+* Follow the instructions at http://wiringpi.com/download-and-install/ to install the WiringPi library on your Pi
+* ```gcc -lwiringPi -o gpio-interrupt gpio-interrupt.c```
+
+Running
+-------
+Must be run as root
+
+```
+./gpio-interrupt
+```
---
+++ gpio-interrupt.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <wiringPi.h>
+
+// Which GPIO pin we're using
+#define PIN 2
+// How much time a change must be since the last in order to count as a change
+#define IGNORE_CHANGE_BELOW_USEC 10000
+
+// Current state of the pin
+static volatile int state;
+// Time of last change
+struct timeval last_change;
+
+// Handler for interrupt
+void handle(void) {
+	struct timeval now;
+	unsigned long diff;
+
+	gettimeofday(&now, NULL);
+
+	if (&last_change) {
+		// Time difference in usec
+		diff = (now.tv_sec * 1000000 + now.tv_usec) - (last_change.tv_sec * 1000000 + last_change.tv_usec);
+
+		// Filter jitter
+		if (diff > IGNORE_CHANGE_BELOW_USEC) {
+			if (state) {
+				printf("Falling\n");
+			}
+			else {
+				printf("Rising\n");
+			}
+
+			state = !state;
+		}
+	}
+
+	last_change = now;
+}
+
+int main(void) {
+	// Init
+	wiringPiSetup();
+
+	// Bind to interrupt
+	wiringPiISR(PIN, INT_EDGE_BOTH, &handle);
+
+	// Set initial state of pin
+	state = digitalRead(PIN);
+
+	if (state) {
+		printf("Started! Initial state is on\n");
+	}
+	else {
+		printf("Started! Initial state is off\n");
+	}
+
+	// Waste time but not CPU
+	for (;;) {
+		sleep(1);
+	}
+}
